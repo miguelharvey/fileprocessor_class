b44e2c8bbe7ae1a054c143a71d69c98ed9c2c05c
Michael Harvey <michael.james.harvey@gmail.com>
2015-07-05 14:53:27 -0400

    Update the Database creation script by removing the database name inthemysql command syntax.

--- db/createDb.sh
+++ db/createDb.sh
@@ -3,4 +3,4 @@ db=$1
 
 testing_data=`cat $2`
 
-echo "drop database if exists ${db}; create database ${db};  use ${db};  $testing_data"  | cat | mysql -p ${db}
+echo "drop database if exists ${db}; create database ${db};  use ${db};  $testing_data"  | cat | mysql -p
